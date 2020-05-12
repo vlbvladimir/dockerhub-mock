86b85b8985e79af206192c1c7406338bb771a0bc
Your Name <you@example.com>
2020-05-12 14:39:50 -0500

    prueba

---
+++ src/main/java/com/colmedica/TesthubApplication.java
@@ -0,0 +1,14 @@
+package com.colmedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesthubApplication {
+
+	public static void main(String[] args) {
+	    System.out.println("holamundo");
+		SpringApplication.run(TesthubApplication.class, args);
+	}
+
+}
